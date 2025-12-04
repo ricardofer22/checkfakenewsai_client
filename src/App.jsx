dfe26d14127687b03889af809c140478f11b9f77
Ricardo <fernandezlorcaricardo@gmail.com>
2025-12-04 14:15:14 -0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Container from './components/Container'
+
+function App() {
+  return (
+    <Container>
+      <h1 className="text-red-500">Hola mundooooo</h1>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+const Container = ({children}) => {
+  return (
+   <div className="bg-gray-800 h-screen w-full">
+      {children}
+   </div> 
+  )
+}
+
+export default Container
